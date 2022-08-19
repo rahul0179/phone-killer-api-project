cd1bfa2704b13bc066032cf6d1a2fb14b1e1dc15
Rahul barman <75018120+rahul0179@users.noreply.github.com>
2022-08-19 21:43:01 +0600

    error massage handle

--- js/phone.js
+++ js/phone.js
@@ -5,6 +5,18 @@ const search = () => {
     console.log(searchText);
     searchInput.value = '';
 
+    if (searchText == '') {
+        const displayPic = document.getElementById("displayPicture")
+        displayPic.innerHTML = '';
+        const error = document.getElementById('error-massage');
+        error.innerHTML = `
+        <h1 class="text-center text-danger">Please enter input in searc bar !!</h1>
+        
+        `
+
+    }
+
+
     //  get data 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
@@ -17,20 +29,23 @@ const displaySearch = searchMobole => {
     const data = searchMobole.data;
     console.log(data)
     const card = document.getElementById('card')
+    const displayPic = document.getElementById("displayPicture")
+    displayPic.innerHTML = '';
+    card.innerHTML = "";
     data.forEach(i => {
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML = `
         
         
-        <div class="card h-100">
-            <img src="${i.image}" class="card-img-top" alt="...">
+        <div class="card h-50">
+            <img src="${i.image}" class="card-img-top p-4 " alt="...">
             <div class="card-body">
                 <h5 class="card-title">${i.phone_name}</h5>
                 <p class="card-text">${i.slug}</p>
             </div>
-            <div class="card-footer align-content-center">
-                <button type="button" class="btn btn-outline-dark">Dark</button>
+            <div class="card-footer d-flex justify-content-center bg-primary">
+                <button type="button" class="btn btn-outline-dark  bg-success">Dark</button>
             </div>
         </div>
         
